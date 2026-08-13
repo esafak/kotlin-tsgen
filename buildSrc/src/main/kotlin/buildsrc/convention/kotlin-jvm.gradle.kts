e8cf0e09416a272af7e2e2931cae582554479c0a
Emre Şafak <3928300+esafak@users.noreply.github.com>
2026-08-13 01:03:29 -0400

    chore: upgrade Kotlin and Gradle

--- buildSrc/src/main/kotlin/buildsrc/convention/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/kotlin-jvm.gradle.kts
@@ -2,7 +2,7 @@ package buildsrc.convention
 
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2
 
 plugins {
   id("buildsrc.convention.base")
@@ -16,8 +16,8 @@ extensions.configure<KotlinJvmProjectExtension> {
 //kotlin {
   jvmToolchain(25)
   compilerOptions {
-    languageVersion = KOTLIN_2_0
-    apiVersion = KOTLIN_2_0
+    languageVersion = KOTLIN_2_2
+    apiVersion = KOTLIN_2_2
     jvmTarget = JvmTarget.JVM_11
     freeCompilerArgs.add(jvmTarget.map { target ->
       "-Xjdk-release=${target.target}"
